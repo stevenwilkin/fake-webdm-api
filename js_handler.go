bdb66a27c98d31221b289a2a6f1c3ef6b6068a94
Steven Wilkin <stevenwilkin@gmail.com>
2016-04-14 15:52:38 +0700

    Cache the processed JavaScript if CACHE_JS is set to "1"

--- js_handler.go
+++ js_handler.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"net/http"
+	"os"
 	"os/exec"
 )
 
-func JsHandler(w http.ResponseWriter, r *http.Request) {
+var (
+	js []byte
+)
+
+func getJS() []byte {
 	out, err := exec.Command("./node_modules/browserify/bin/cmd.js", "-t", "hbsfy", "./js/app.js").Output()
 	if err != nil {
 		panic(err)
 	}
+	return out
+}
+
+func JsHandler(w http.ResponseWriter, r *http.Request) {
+	if os.Getenv("CACHE_JS") != "1" || len(js) == 0 {
+		js = getJS()
+	}
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(out)
+	w.Write(js)
 }
