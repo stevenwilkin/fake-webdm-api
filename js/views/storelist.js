26019091dc266e41f49bc42630d79b5dd2ea2109
Steven Wilkin <stevenwilkin@gmail.com>
2016-04-18 16:11:26 +0700

    First cut of applying new design to store page

--- js/views/storelist.js
+++ js/views/storelist.js
@@ -1,7 +1,12 @@
 // storelist view
 var Snaplist = require('./snaplist.js');
 var StorelistItemView = require('./storelist-item.js');
+var template = require('../templates/storelist.hbs');
 
 module.exports = Snaplist.extend({
-  childView: StorelistItemView
+  childView: StorelistItemView,
+
+  template : function(model) {
+    return template(model);
+  }
 });
